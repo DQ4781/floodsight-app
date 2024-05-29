23e427e892e55a4cab08ead9701bdc88acef44f9
Daniel Quezada <dquezada478@gmail.com>
2024-05-29 09:33:27 -0700

    Updated colors and Route Names

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import EmergencyInfo from './pages/EmergencyInfo';
+import EmergencyInfoTabs from './pages/EmergencyInfo';
 import './styles/App.css';
 import BottomSection from './components/BottomSection';
 import TopSection from './components/TopSection';
 
 const App = () => {
   return (
-    <div style={{ backgroundColor: 'rgb(0, 0, 0)' }} className="text-white min-h-screen flex flex-col">
+    <div className="bg-black text-white min-h-screen flex flex-col">
       <Router>
         <TopSection />
         <main className="flex-grow pt-16 pb-16">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/emergency-info" element={<EmergencyInfo />} />
+            <Route path="/emergency-info" element={<EmergencyInfoTabs />} />
           </Routes>
         </main>
         <BottomSection />
